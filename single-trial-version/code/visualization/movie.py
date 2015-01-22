6e814386d883bb2ef2394e6c328703b2f987a408
Antonio Ulloa <antonio.ulloa@alum.bu.edu>
2015-01-22 12:48:52 -0500

    added animation of output data w interactive slider

--- single-trial-version/code/visualization/movie.py
+++ single-trial-version/code/visualization/movie.py
@@ -47,8 +47,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button, RadioButtons
 
-i=75
-
 # Load data files
 lgns = np.loadtxt('../../output/lgns.out')
 efd1 = np.loadtxt('../../output/efd1.out')
@@ -64,18 +62,19 @@ exss = np.loadtxt('../../output/exss.out')
 
 plt.figure(1)
 
+print lgns.shape
+
 # Render LGN array in a colormap
 ax = plt.subplot(3,6,7)
-lgns = lgns[i]
-lgns = lgns.reshape(9, 9)
-img = plt.imshow(lgns)
+lgn = lgns[0]
+img = plt.imshow(lgn.reshape(9,9))
 img.set_cmap('hot')
 ax.set_xticks([])
 ax.set_yticks([])
 
 # Render EV1h array in a colormap
 ax=plt.subplot(3,6,14)
-ev1h = ev1h[i]
+ev1h = ev1h[0]
 ev1h = ev1h.reshape(9, 9)
 img = plt.imshow(ev1h)
 img.set_cmap('hot')
@@ -84,7 +83,7 @@ ax.set_yticks([])
 
 # Render EV1v array in a colormap
 ax=plt.subplot(3,6,2)
-ev1v = ev1v[i]
+ev1v = ev1v[0]
 ev1v = ev1v.reshape(9, 9)
 img = plt.imshow(ev1v)
 img.set_cmap('hot')
@@ -93,7 +92,7 @@ ax.set_yticks([])
 
 # Render array in a colormap
 ax=plt.subplot(3,6,3)
-ev4h = ev4h[i]
+ev4h = ev4h[0]
 ev4h = ev4h.reshape(9, 9)
 img = plt.imshow(ev4h)
 img.set_cmap('hot')
@@ -102,7 +101,7 @@ ax.set_yticks([])
 
 # Render array in a colormap
 ax=plt.subplot(3,6,9)
-ev4c = ev4c[i]
+ev4c = ev4c[0]
 ev4c = ev4c.reshape(9, 9)
 img = plt.imshow(ev4c)
 img.set_cmap('hot')
@@ -111,7 +110,7 @@ ax.set_yticks([])
 
 # Render array in a colormap
 ax=plt.subplot(3,6,15)
-ev4v = ev4v[75]
+ev4v = ev4v[0]
 ev4v = ev4v.reshape(9, 9)
 img = plt.imshow(ev4v)
 img.set_cmap('hot')
@@ -120,7 +119,7 @@ ax.set_yticks([])
 
 # Render array in a colormap
 ax=plt.subplot(3,6,10)
-exss = exss[i]
+exss = exss[0]
 exss = exss.reshape(9, 9)
 img = plt.imshow(exss)
 img.set_cmap('hot')
@@ -129,7 +128,7 @@ ax.set_yticks([])
 
 # Render array in a colormap
 ax=plt.subplot(3,6,5)
-exfs = exfs[i]
+exfs = exfs[0]
 exfs = exfs.reshape(9, 9)
 img = plt.imshow(exfs)
 img.set_cmap('hot')
@@ -138,7 +137,7 @@ ax.set_yticks([])
 
 # Render array in a colormap
 ax=plt.subplot(3,6,11)
-efd1 = efd1[i]
+efd1 = efd1[0]
 efd1 = efd1.reshape(9, 9)
 img = plt.imshow(efd1)
 img.set_cmap('hot')
@@ -147,7 +146,7 @@ ax.set_yticks([])
 
 # Render array in a colormap
 ax=plt.subplot(3,6,17)
-efd2 = efd2[75]
+efd2 = efd2[0]
 efd2 = efd2.reshape(9, 9)
 img = plt.imshow(efd2)
 img.set_cmap('hot')
@@ -156,7 +155,7 @@ ax.set_yticks([])
 
 # Render array in a colormap
 ax=plt.subplot(3,6,12)
-exfr = exfr[i]
+exfr = exfr[0]
 exfr = exfr.reshape(9, 9)
 img = plt.imshow(exfr)
 img.set_cmap('hot')
@@ -166,12 +165,87 @@ ax.set_yticks([])
 # now draw interactive slider
 axtimesteps = plt.axes([0.25, 0.1, 0.65, 0.03])
 
-stimesteps = Slider(axtimesteps, 'TimeSteps', 0, 220, valinit=75)
+stimesteps = Slider(axtimesteps, 'TimeSteps', 0, 219, valinit=0)
 
 def update(val):
     timesteps = stimesteps.val
-    ax.set_xdata(timesteps)
-    fig.canvas.draw_idle()
+
+    ax = plt.subplot(3,6,7)
+    lgn = lgns[timesteps]
+    img = plt.imshow(lgn.reshape(9,9))
+    img.set_cmap('hot')
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    ax=plt.subplot(3,6,14)
+    v1h = ev1h[timesteps]
+    img = plt.imshow(v1h.reshape(9,9))
+    img.set_cmap('hot')
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    ax=plt.subplot(3,6,2)
+    v1v = ev1v[timesteps]
+    img = plt.imshow(ev1v)
+    img.set_cmap('hot')
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    ax=plt.subplot(3,6,3)
+    v4h = ev4h[timesteps]
+    img = plt.imshow(ev4h)
+    img.set_cmap('hot')
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    ax=plt.subplot(3,6,9)
+    v4c = ev4c[timesteps]
+    img = plt.imshow(v4c.reshape(9,9))
+    img.set_cmap('hot')
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    ax=plt.subplot(3,6,15)
+    v4v = ev4v[timesteps]
+    img = plt.imshow(v4v.reshape(9,9))
+    img.set_cmap('hot')
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    ax=plt.subplot(3,6,10)
+    ss = exss[timesteps]
+    img = plt.imshow(ss.reshape(9,9))
+    img.set_cmap('hot')
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    ax=plt.subplot(3,6,5)
+    fs = exfs[timesteps]
+    img = plt.imshow(fs.reshape(9,9))
+    img.set_cmap('hot')
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    ax=plt.subplot(3,6,11)
+    fd1 = efd1[timesteps]
+    img = plt.imshow(fd1.reshape(9,9))
+    img.set_cmap('hot')
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    ax=plt.subplot(3,6,17)
+    fd2 = efd2[timesteps]
+    img = plt.imshow(fd2.reshape(9,9))
+    img.set_cmap('hot')
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    ax=plt.subplot(3,6,12)
+    fr = exfr[timesteps]
+    img = plt.imshow(fr.reshape(9,9))
+    img.set_cmap('hot')
+    ax.set_xticks([])
+    ax.set_yticks([])
 
 stimesteps.on_changed(update)
 
