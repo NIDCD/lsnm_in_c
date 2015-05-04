fixed bugs in fmri calculation

python/code/visualization/fmriVisual.py
@@ -56,11 +56,11 @@ from scipy.stats import poisson
 from scipy import signal
 
 # define constants needed for hemodynamic function
-lambda_ = 4.0
+lambda_ = 6.0
 
 # given the number of total timesteps, calculate total time of scanning
 # experiment in seconds
-T = 22
+T = 266
 
 # Time for one complete trial
 Ttrial = 5.5

python/code/visualization/plotSynapticVisual.py
@@ -62,8 +62,8 @@ efd1 = np.loadtxt('../../output/efd1_synaptic.out')
 ifd1 = np.loadtxt('../../output/ifd1_synaptic.out')
 
 # Load R synaptic activity data files into a numpy array
-exfr = np.loadtxt('../../output/exfr_synaptic.out')
-infr = np.loadtxt('../../output/infr_synaptic.out')
+#exfr = np.loadtxt('../../output/exfr_synaptic.out')
+#infr = np.loadtxt('../../output/infr_synaptic.out')
 
 # Extract number of timesteps from one of the matrices
 timesteps = ev1h.shape[0]
@@ -75,7 +75,7 @@ t = np.arange(0, timesteps, 1)
 v1 = np.sum(ev1h + ev1v + iv1h + iv1v, axis = 1)
 it = np.sum(exss + inss, axis = 1)
 d1 = np.sum(efd1 + ifd1, axis = 1)
-fr = np.sum(exfr + infr, axis = 1)
+#fr = np.sum(exfr + infr, axis = 1)
 
 # Set up plot
 plt.figure(1)
@@ -86,7 +86,7 @@ plt.suptitle('SIMULATED SYNAPTIC ACTIVITY')
 plt.plot(t, v1)
 plt.plot(t, it)
 plt.plot(t, d1)
-plt.plot(t, fr)
+#plt.plot(t, fr)
 
 # Show the plot on the screen
 plt.show()